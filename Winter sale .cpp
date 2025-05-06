c437b8c74a8c432371b170f45ec17cb655f95a55
Abdo803423 <adlabdo888@gmail.com>
2025-05-07 02:14:47 +0300

    Create Winter sale .cpp

---
+++ Winter sale .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float P;
+    float X;
+    cin >> X>> P;
+    float res = P*(100/(100-X));   
+    cout << res<< endl; 
+    return 0;
+}
